08e215f959b194cacaf6f161375f4346fe5e76d1
Роман Леонтьев <roman_leon1995@outlook.com>
2021-10-24 20:15:02 +0600

    Планируемые эндпоинты

--- src/modules/accounts/accounts.controller.ts
+++ src/modules/accounts/accounts.controller.ts
@@ -1,6 +1,25 @@
 import {Controller} from "@nestjs/common";
+import {MessagePattern} from "@nestjs/microservices";
 
 @Controller()
 export class AccountsController {
+
+  /** Информация юзера, не привязанная к блокчейну,  */
+  @MessagePattern(`server.account.getUser`)
+  async getUserByTelegramId() {}
+
+  /** Адрес аккаунта и его memo для пополнения баланса юзера */
+  @MessagePattern(`server.account.getDepositAddress`)
+  async getDepositAddress() {}
+
+  /** Создание нового аккаунта в eosio и привязка к юзеру */
+  @MessagePattern(`server.account.createEos`)
+  async createEosioAccount() {}
+
+  /** Eosio аккаунты юзеры */
+  @MessagePattern(`server.account.getUserEos`)
+  async getUserEosioAccounts() {
+    // у каждого аккаунта есть список активных игр
+  }
 
 }
--- src/modules/core/core.controller.ts
+++ src/modules/core/core.controller.ts
@@ -1,6 +1,41 @@
 import {Controller} from "@nestjs/common";
+import {MessagePattern} from "@nestjs/microservices";
 
 @Controller()
 export class CoreController {
+
+  /** Блокчейн боты юзера */
+  @MessagePattern(`farmersworld.bot.getUserBots`)
+  async getUserBots() {
+    // [cpu, ram, net], in game balance, tokens balance, in game stats (energy and tools conditions)
+  }
+
+  /** Получить определенного бота и данные по заданным фильтрам */
+  @MessagePattern(`farmersworld.bot.getBotById`)
+  async getBotById() {}
+
+  /** Остановка работы бота в игре */
+  @MessagePattern(`farmersworld.bot.stopById`)
+  async stopBotById() {}
+
+  /** Запуск бота в игре */
+  @MessagePattern(`farmersworld.bot.startById`)
+  async startBotById() {}
+
+  /** Депозит предмета с блокчейна в игру */
+  @MessagePattern(`farmersworld.bot.stakeTool`)
+  async stakeBotTool() {}
+
+  /** Вывод предмета из в игры на блокчейн аккаунт */
+  @MessagePattern(`farmersworld.bot.unstakeTool`)
+  async unstakeBotTool() {}
+
+  /** Вывод wax баланса */
+  @MessagePattern(`farmersworld.bot.sendToken`)
+  async sendToken() {}
+
+  /** Вывод NFT токена */
+  @MessagePattern(`farmersworld.bot.sendNft`)
+  async sendNft() {}
 
 }
